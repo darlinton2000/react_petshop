e659d29e1ada0bb631a3d76d743ac435bb5bd41a
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2025-02-19 22:35:36 -0300

    feat: component about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import {Hero} from "@/app/_components/hero";
+import { Hero } from "@/app/_components/hero";
+import { About } from "@/app/_components/about";
 
 export default function Home() {
   return(
       <main>
           <Hero />
+          <About />
       </main>
   )
 }
