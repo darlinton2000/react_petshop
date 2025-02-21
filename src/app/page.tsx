d099a3b518ad00de6ad709f41d9e32fb0bb464ef
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2025-02-20 21:00:19 -0300

    feat: component services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/app/_components/hero";
 import { About } from "@/app/_components/about";
+import { Services } from "@/app/_components/services";
 
 export default function Home() {
   return(
       <main>
           <Hero />
           <About />
+          <Services />
       </main>
   )
 }
