1505c08bb2e8fb66d7d384e079caeccda4fe4588
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2025-02-23 09:24:30 -0300

    feat: effects on components

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Image from "next/image"
 import about1Img from '../../../public/images/about-1.png'
 import about2Img from '../../../public/images/about-2.png'
-import { Check, MapPin } from "lucide-react";
+import { Check, MapPin } from "lucide-react"
 import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr"
 
 export function About() {
@@ -9,7 +9,7 @@ export function About() {
         <section className="bg-[#FDF6EC] py-16">
             <div className="container px-4 mx-auto">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-                    <div className="relative">
+                    <div className="relative" data-aos="fade-up-right" data-aos-delay="300">
                         <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
                             <Image
                                 src={about1Img}
@@ -30,7 +30,7 @@ export function About() {
                             />
                         </div>
                     </div>
-                    <div className="space-y-6 mt-10">
+                    <div className="space-y-6 mt-10" data-aos="fade-up-left" data-aos-delay="300">
                         <h2 className="text-4xl font-bold">Sobre</h2>
                         <p>
                             Until one has loved an animal, a part of one's soul remains unawakened. We believe in it and we
